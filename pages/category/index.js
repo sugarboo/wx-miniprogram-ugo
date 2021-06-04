c84d316e5bf00e62ef3febada160e84dc6bf3e79
sugarboo <kennethbaak@gmail.com>
2021-06-04 17:03:40 +0800

    category completed

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,59 @@
 // pages/category/index.js
+
+// 引入request和runtime
+import { request } from "../../utils/request/index.js"
+import regeneratorRuntime from '../../lib/runtime/runtime'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [], // 存放左侧菜单数据的数组, 默认值为空
+    rightContentList: [], // 存放右侧内容数据的数组, 默认值为空
+    currentIndex: 0, // 当前菜单选项的索引, 默认值为0
+    isScrollTop: 0 // 右侧内容滚动条距离顶部的距离, 表示是否滚动至顶部, 默认为是, 即0
   },
+  categories: [], // 存放分类数据的数组, 默认值为空
 
   /**
    * 生命周期函数--监听页面加载
+   * 当页面开始加载时, 发送网络请求, 获取数据
    */
   onLoad: function (options) {
-
+    this.getCategory()
+  },
+  
+  /**
+   * 发送网络请求, 获取分类数据
+   */
+  async getCategory () {
+    const res = await request({
+      url: "/categories"
+    })
+    // 请求成功响应, 将响应的分类数据保存在data对应的变量中
+    this.categories = res.data.message
+    console.log(this.categories)
+    // 构造左侧菜单数据
+    let leftMenuList = this.categories.map(v => v.cat_name)
+    // 构造右侧内容数据
+    let rightContentList = this.categories[0].children
+    this.setData({
+      leftMenuList,
+      rightContentList
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 左侧菜单选项的点击事件监听处理
+  handleMenuItemTap (e) {
+    // 获取当前点击的菜单选项的索引
+    const {index} = e.currentTarget.dataset
+    // 将获取到的索引保存至currentIndex中
+    this.setData({
+      currentIndex: index,
+      rightContentList: this.categories[index].children,
+      isScrollTop: 0 // 重新设置右侧内容的scroll-view标签的距离顶部的距离
+    })
   }
 })
--- utils/request/index.js
+++ utils/request/index.js
@@ -2,6 +2,9 @@
  * 封装网络请求模块
  */
 
+// 同时发送的异步请求数量
+let ajaxCounts = 0
+
 /**
  * 使用Promise封装请求
  */
@@ -10,15 +13,31 @@
   //  定义公共url
   const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1"
 
+  // 当request被调用时, 页面发送了异步请求
+  ajaxCounts++
+
+  // 开启加载中
+  wx.showLoading({
+    title: "加载中",
+    mask: true,
+  })
+
    return new Promise((resolve, reject) => {
      wx.request({
        ...params,
        url: baseUrl + params.url,
-       success: (result) => {
+       success: result => {
          resolve(result)
        },
-       fail: (err) => {
+       fail: err => {
          reject(err)
+       },
+       complete: () => {
+        ajaxCounts--
+        if (ajaxCounts == 0) {
+          // 关闭加载中
+          wx.hideLoading()
+        }
        }
      })
    })
